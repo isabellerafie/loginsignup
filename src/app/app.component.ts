1b5e4433be1f09ae94f682f0216e1c2e9fe72658
Isabelle El Rafih <isorafi3@live.com>
2024-05-07 23:59:16 +0300

    YESSSSSSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  isSignUpVisible: boolean = true;
+  overlayLeft: string = '77%'; // Initial position for sign up
+
+  showLoginForm() {
+    this.isSignUpVisible = false;
+    this.overlayLeft = '20%'; // Move overlay to show login
+  }
+
+  showSignUpForm() {
+    this.isSignUpVisible = true;
+    this.overlayLeft = '77%'; // Move overlay to show sign up
+  }
 }
